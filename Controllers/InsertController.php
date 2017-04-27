d67095332d441762716451028d153b89f90a995b
Sergei <skorohods@mail.ru>
2017-04-27 09:27:44 +0300

    Working engine

---
+++ Controllers/InsertController.php
@@ -0,0 +1,9 @@
+<?php
+    namespace App;
+
+
+    class InsertController
+    {   public function index() {
+        echo 'Hello';
+        }
+    }
---
+++ Controllers/MainController.php
@@ -0,0 +1,12 @@
+<?php
+    namespace App;
+
+    class MainController
+    {
+        public function index (){
+
+            require "views/index.php";
+
+        }
+
+    }
